99f77b236c25b23eeaf33e0044e15fe80a062bb9
Ken Crocken <kcrocken@gmail.com>
2014-08-28 00:01:21 -0400

    add abv

--- app/beer.rb
+++ app/beer.rb
@@ -1,11 +1,12 @@
 class Beer
-  attr_accessor :name, :image_url, :description
+  attr_accessor :name, :image_url, :description, :abv
 
   def self.from_json(json)
     Beer.new.tap do |b|
       b.name = json["name"]
       b.description = json["description"]
       b.image_url = json["labels"]["medium"] if json["labels"]
+      b.abv = json["abv"]
     end
   end
 end
